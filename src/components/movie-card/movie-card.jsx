0b5f0478353c06813f56ef22dc870581598c2693
GuyRimel <135guyrimel@gmail.com>
2022-11-14 17:50:38 -0600

    style changes, favorite movies button working

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Image } from "react-bootstrap";
 import "./movie-card.scss";
 
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
